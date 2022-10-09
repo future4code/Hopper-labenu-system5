5679c861413b3630cadff074a02e3653f3d59f14
bruno-macena <bdsm4@hotmail.com>
2022-10-09 15:17:41 -0300

    Correção endpoint

--- src/database/DocenteDatabase.ts
+++ src/database/DocenteDatabase.ts
@@ -3,16 +3,16 @@ import { BaseDatabase } from "./BaseDatabase";
 import { TABELA_DOCENTE } from "./tableNames";
 
 export class DocenteDataBase extends BaseDatabase {
-    public async criarDocente(especialidades: Docente) {
-        await BaseDatabase.connection(TABELA_DOCENTE).insert({
-            id: especialidades.getId(),
-            nome: especialidades.getNome(),
-            email: especialidades.getEmail(),
-            data_nasc: especialidades.getDataNasc()
-        })
+    // public async criarDocente(especialidades: Docente) {
+    //     await BaseDatabase.connection(TABELA_DOCENTE).insert({
+    //         id: especialidades.getId(),
+    //         nome: especialidades.getNome(),
+    //         email: especialidades.getEmail(),
+    //         data_nasc: especialidades.getDataNasc()
+    //     })
 
 
-    }
+    // }
     public async getDocente() {
         const result = await BaseDatabase.connection(TABELA_DOCENTE)
         return result
